fd41d622f19ba752fb0cddffe17ac8c1697babc3
yashmodi998 <yashmodi998@gmail.com>
2024-01-23 13:39:31 -0500

    first commit

---
+++ app/src/main/java/com/example/loginwithsharedpref/modelClass/URLs.java
@@ -0,0 +1,10 @@
+package com.example.loginwithsharedpref.modelClass;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://192.168.0.100/login/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "login";
+
+}
